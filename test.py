9857ca6652ce321111af70c8aa88bfcd1fdd8186
M Ridho NF <ridhoamstrong4@gmail.com>
2025-05-14 21:30:54 +0700

    Update test.py

--- test.py
+++ test.py
@@ -3,7 +3,7 @@ import sympy as sp
 import numpy as np
 import matplotlib.pyplot as plt
 from mpl_toolkits.mplot3d import Axes3D
-except as e:
+except Exception as e
 
 # --- Simbol ---
 L, K = sp.symbols('L K')
